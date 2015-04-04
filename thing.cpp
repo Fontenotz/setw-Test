f4f73382934c4b4822ac8e44d32c29903de7b98d
Mike Dank <Famicoman@gmail.com>
2015-04-04 15:26:11 -0400

    Initial commit.
    Initial commit.

---
+++ thing.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<stdlib.h>
+#include<iomanip.h>
+
+
+int x=0;
+int main()
+{
+	cout<<setw(x)<<"Computer Programming is the REAL Deal"<<endl;
+	x++;
+	system("cls");
+	main();
+
+	return 0;
+}
+
